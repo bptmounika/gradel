eskatos/gradle

Deprecate loading test framework implementation dependencies from the distribution
Users will need to include the test framework dependencies as a runtime dependency

subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/JUnitIntegrationTest.groovy
@@ -402,7 +402,10 @@ class JUnitIntegrationTest extends JUnitMultiVersionIntegrationSpec {
         testDirectory.file('build.gradle') << """
             apply plugin: 'java'
             ${mavenCentralRepository()}
-            dependencies { testImplementation 'junit:junit:3.8' }
+            dependencies {
+                testImplementation 'junit:junit:3.8'
+                testRuntimeOnly 'junit:junit:4.13.2'
+            }
             def listener = new TestListenerImpl()
             test.addTestListener(listener)
             test.ignoreFailures = true
@@ -468,7 +471,7 @@ class JUnitIntegrationTest extends JUnitMultiVersionIntegrationSpec {
             apply plugin: 'java'
             ${mavenCentralRepository()}
             dependencies {
-                ${dependencyNotation.collect { "testImplementation '$it'" }.join('\n')}
+                ${getDependencyBlockContents()}
             }
         """
 

bptmounika/gradel

subprojects/testing-jvm/src/integTest/groovy/org/gradle/testing/junit/JUnitIntegrationTest.groovy
@@ -402,7 +402,10 @@ class JUnitIntegrationTest extends JUnitMultiVersionIntegrationSpec {
         testDirectory.file('build.gradle') << """
             apply plugin: 'java'
             ${mavenCentralRepository()}
-            dependencies { testImplementation 'junit:junit:3.8' }
+            dependencies {
+                testCompileOnly 'junit:junit:3.8'
+                testRuntimeOnly 'junit:junit:4.13.2'
+            }
             def listener = new TestListenerImpl()
             test.addTestListener(listener)
             test.ignoreFailures = true
@@ -468,7 +471,7 @@ class JUnitIntegrationTest extends JUnitMultiVersionIntegrationSpec {
             apply plugin: 'java'
             ${mavenCentralRepository()}
             dependencies {
-                ${dependencyNotation.collect { "testImplementation '$it'" }.join('\n')}
+                ${getDependencyBlockContents()}
             }
         """
 